589a9acab92af09ef4b0e2aceed2dc938953ac0f
RAJ KUMAR <rajveerroy8674@gmail.com>
2024-07-20 12:05:58 +0000

    data type

---
+++ 1-basics/02-variable.js
@@ -0,0 +1,12 @@
+const accountId = 867493
+let accountEmail = "rajveerroy8674@ gamil.com"
+var accountPassword = "1234"
+accountCity = "jaipur"
+
+// accountId = 7451
+accountEmail= "kraj7584@gmail.com"
+accountPassword="7496"
+accountCity="delhi"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 1-basics/03-data_type.js
@@ -0,0 +1,21 @@
+"use strict"; //treat all js code in newer version
+
+let name = "raj kumar"
+let age  = 22
+let isloggedIn = false
+let state;
+
+/*type of data tpye
+number => 2 to power 53
+bigint
+string = ""
+boollean = true or false
+null standalone value
+undefinded value = >
+symbol= for unique
+
+object
+*/
+console.log(typeof"raj kumar")
+
+console.log(typeof age)
