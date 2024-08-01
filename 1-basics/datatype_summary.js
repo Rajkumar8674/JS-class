70c1a9c7ce8bcd110d4f40b46c90340618797e95
RAJ KUMAR <rajveerroy8674@gmail.com>
2024-08-01 07:06:54 +0000

    datatype-summary

--- 1-basics/datatype_summary.js
+++ 1-basics/datatype_summary.js
@@ -5,6 +5,11 @@
 
 // reference (non-primitive datatype)= array,function,object:
 
+// memory= stack(primitive) And heap(non-primitive)
+// stack is a copy
+// heap is  reference value
+
+
 const Id=Symbol('123')
 const anotherID=Symbol('123')
 console.log(Id==anotherID);
