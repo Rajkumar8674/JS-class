6d57fd74a2e3b1e85882572a1f92b5d2f3f6bec1
RAJ KUMAR <rajveerroy8674@gmail.com>
2024-08-01 06:28:18 +0000

    datatypesummary

---
+++ 1-basics/datatype_summary.js
@@ -0,0 +1,18 @@
+//primitive
+
+//7 type= string, number , boolean,null, undefine,symbol,BigInt
+
+
+// reference (non-primitive datatype)= array,function,object:
+
+const Id=Symbol('123')
+const anotherID=Symbol('123')
+console.log(Id==anotherID);
+
+
+const heros = ["thor,ironman,deadpool"];
+
+let object = {
+    name:"raj kumar"
+    age: 20,
+}
