e7dec532d4d3c79cdb0506950505a9ce12b26f64
RAJ KUMAR <rajveerroy8674@gmail.com>
2024-08-01 07:57:41 +0000

    string-class

---
+++ 1-basics/04-string.js
@@ -0,0 +1,15 @@
+const name = "raj kumar"
+age =20;
+
+console.log(`my name is ${name} and my age is ${age}`)
+
+const gameName = new String('battlegrouondplay')
+console.log(gameName[0])
+console.log(gameName.length)
+console.log(gameName.toUpperCase())
+console.log(gameName.charAt(10))
+console.log(gameName.indexOf('p'))
+const newString = gameName.substring(0,8)
+console.log(newString)
+const sliceSting = gameName.slice(-13,8)
+console.log(sliceSting)
