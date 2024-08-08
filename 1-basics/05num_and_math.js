bea8d00417350c22772bd70733a66c388d8bab80
RAJ KUMAR <rajveerroy8674@gmail.com>
2024-08-08 18:02:23 +0000

    mathandnumbers

---
+++ 1-basics/05num_and_math.js
@@ -0,0 +1,27 @@
+const score = 400
+// console.log(score)
+const balance = new Number (100)
+// console.log(balance)
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(1))
+
+const OtherNumber = 125.1452
+// console.log(OtherNumber.toPrecision(5))
+
+const hunders = 1000000
+// console.log(hunders.toLocaleString('en-IN'))
+
+
+
+// ++++++++++++++++++++++++++++ M Ath    ++++++++++++++++++++++++++++++++++++++
+//  console.log(Math)
+//  console.log(Math.abs(-4))
+//  console.log(Math.round(45.5))
+//  console.log(Math.round(45.4))
+//  console.log(Math.ceil(4.5))
+//  console.log(Math.floor(4.5))
+
+//  console.log(Math.min(4,5,8,6,4,7,1))
+//  console.log(Math.max(8,6,4,9,5,2,7))
+
