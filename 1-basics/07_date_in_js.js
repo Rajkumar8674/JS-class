5d27b8e1808436339aa0d63686d71a5dc0ea0a0f
RAJ KUMAR <rajveerroy8674@gmail.com>
2024-08-08 19:45:57 +0000

    dateinjavascripte

---
+++ 1-basics/07_date_in_js.js
@@ -0,0 +1,22 @@
+// let Mydate = new Date()
+// console.log(Mydate);
+// console.log(Mydate.toString());
+// console.log(Mydate.toDateString());
+// console.log(Mydate.toISOString());
+// console.log(Mydate.toJSON());
+// console.log(Mydate.toLocaleString())
+// console.log(typeof Mydate);
+
+
+// let CreateMyDate = new Date(2001,1,14 ,8,30)
+// console.log(CreateMyDate.toDateString())
+// console.log(CreateMyDate.toLocaleString())
+
+let CreateMyDate = new Date(2001,1,14)
+
+let MyTimeStamp = Date.now()
+// console.log(MyTimeStamp)
+// console.log(CreateMyDate.getTime())
+// console.log(CreateMyDate.getTime())
+
+console.log(Math.floor(Date.now()/1000));
