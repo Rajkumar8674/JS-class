f8fd98e1bcfa1e8faf178b4f241fb80d58623326
RAJ KUMAR <rajveerroy8674@gmail.com>
2024-08-09 07:08:29 +0000

    basicsArrays

---
+++ 2_basics/01_arrays.js
@@ -0,0 +1,41 @@
+// const MyArr = [1,2,3,4,5]
+// console.log(MyArr[1]);
+// console.log(MyArr.length)
+// console.log(MyArr.indexOf(4))
+
+// const Myheros = ["thor","iron man","hulk"]
+// console.log(Myheros[2])
+
+// const MyArr2 = new Array(6,5,8,45,3)
+// console.log(MyArr2[2])
+
+
+// ########################### Method arrays #############$%$$%#############
+
+// const MyArr3 = [1,2,3,4,5]
+// MyArr3.push(7);
+// console.log(MyArr3);
+// MyArr3.pop()
+// console.log(MyArr3)
+
+
+// console.log(MyArr3.includes(5))
+// console.log(MyArr3.includes(8))
+// console.log(MyArr3.indexOf(2))
+
+
+// %#%#$%#$%#$%#$#%#$%#%$#%$%  slice AND splice @@@#$##############$@#@@@@@@@@@@@
+
+const MyArr4 = [ "A ",1,2,3,4,5]
+console.log(MyArr4)
+
+const MyNewArray1 = MyArr4.slice(1,4);
+console.log(MyNewArray1);
+ console.log("B",MyArr4);
+
+
+ const MyNewArray2 = MyArr4.splice(1,4);
+console.log(MyNewArray2);
+ console.log("C",MyArr4);
+
+
